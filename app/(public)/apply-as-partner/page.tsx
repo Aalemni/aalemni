5e59455536a63f31b1517e26c4090c5cc5998045
khodor11 <khodoralameh01@outlook.com>
2025-06-17 21:31:45 +0300

    new changes related to partner

--- app/(public)/apply-as-partner/page.tsx
+++ app/(public)/apply-as-partner/page.tsx
@@ -1,9 +1,10 @@
 "use server";
+
 import { createClient } from "@/supabase/utils/server";
 import ApplyAsPartnerPage from "@/components/apply_partner/apply_partner";
 import { getAllPartnershipTypes } from "@/supabase/actions/partners_action";
-import React from "react";
 import { getLoggedInUser } from "@/supabase/actions/user_actions";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
   const { data: partnership_types } = await getAllPartnershipTypes();
@@ -13,17 +14,15 @@ export default async function Page() {
     data: { user },
   } = await supabase.auth.getUser();
 
-  // let logged_in_user = null;
-
   if (!user) {
-    throw new Error("User not logged in");
+    return redirect("/login");
   }
 
   const logged_in_user_res = await getLoggedInUser(user.id);
   const logged_in_user = logged_in_user_res.data?.[0];
 
   if (!logged_in_user) {
-    throw new Error("Logged in user data not found");
+    return redirect("/login");
   }
 
   return (
